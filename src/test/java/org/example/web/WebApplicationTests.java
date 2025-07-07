605fe84b6bd9969a13bcbfb500383f5335f7dd8d
QuirkyTurtle1 <chochua.03@mail.ru>
2025-07-07 12:38:19 +0300

    asd

--- src/test/java/org/example/web/WebApplicationTests.java
+++ src/test/java/org/example/web/WebApplicationTests.java
@@ -3,11 +3,11 @@ package org.example.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WebApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class WebApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
